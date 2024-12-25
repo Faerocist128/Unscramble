1f2841c7fce4d4d7ea0b413813e5cc0dc1e0c2fe
Faerocist128 <vasinsr12@gmail.com>
2024-12-25 21:43:43 +0000

    commenting

--- Code.cpp
+++ Code.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int p = 5;
+/*
+p is the precision constant. Because of round off errors, the numerical values for words and scrambled words can differ.
+The p value tells the program to return all strings in the map on the range [d - p*1e-15, d + p*1e-15], where
+d is the double value of the input string.
+*/
+
 int main() {
     
-    map<double, vector<string>>arr;
+    unordered_map<double, vector<string>>arr;
 
     ifstream F("words_alpha.txt");
 
@@ -11,12 +18,12 @@ int main() {
 
     cout << "Initializing ..." << endl << endl;
 
-    while (getline(F, s)) {
+    while (getline(F, s)) {//Calculating double values and pushing words from word list into arr
         double d = 0;
         transform(s.begin(), s.end(), s.begin(), ::tolower);
 
         for (char c : s) {
-            if ((c - 'a') < 26 && (c - 'a') >= 0) {
+            if ((c - 'a') < 26 && (c - 'a') >= 0) {//getline was picking up \n as a character
                 d += exp(1.0/(int)(c - '`'));
             }
         }
@@ -28,20 +35,20 @@ int main() {
     cout << "The string cannot include numbers, punctuation, or any non-alphabetical characters." << endl;
     cout << "Reply \"1\" to end the program." << endl;
    
-
+    //USER INPUTS
     while (true) {
         cout << endl << "input >> ";
         cin >> s;
         
-        if (s.size() == 1 && s == "1") {
+        if (s.size() == 1 && s == "1") {//STOP the program
             break;
         }
         double d = 0;
         transform(s.begin(), s.end(), s.begin(), ::tolower);
 
-        for (char c : s) {
+        for (char c : s) {//calculating double value
             
-            if ((c - 'a') >= 26 || (c - 'a') < 0) {
+            if ((c - 'a') >= 26 || (c - 'a') < 0) {//should be only alphabetical characters
                 d = 0;
                 break;
             }
@@ -54,19 +61,20 @@ int main() {
             continue;
         }
 
-        if (arr[d].size() == 0 && arr[d + 1e-15].size() == 0 && arr[d - 1e-15].size() == 0) {
+        bool good = false;//does the input have any matches?
+        for (int i = -p; i <= p; i ++) {
+            good = (good || (arr[d + i*1e-15].size() != 0));
+        }
+
+        if (!good) {
             cout << "No Matches" << endl;
         }
         else {
             cout << "Matches:" << endl;
-            for (string i : arr[d]) {
-                cout << i << endl;
-            }
-            for (string i : arr[d + 1e-15]) {
-                cout << i << endl;
-            }
-            for (string i : arr[d - 1e-15]) {
-                cout << i << endl;
+            for (int j = -p; j <= p; j ++) {
+                for (string i : arr[d + j*(1e-15)]) {
+                    cout << i << endl;
+                }
             }
 
         }
